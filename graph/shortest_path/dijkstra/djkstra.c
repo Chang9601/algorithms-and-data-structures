8347dcd176d3f2395ca5f47f037e7505b5f09129
Chang Seob <changsup96@gmail.com>
2022-04-13 19:57:06 +0900

    Implement Dijkstra's algorithm.

--- graph/shortest_path/dijkstra/djkstra.c
+++ graph/shortest_path/dijkstra/djkstra.c
@@ -6,58 +6,55 @@
 #define INF 2000000000
 int parent_vertex[MAX_VERTEX + 1];
 
-int prim(Graph *graph, int start_vertex)
+int* dijkstra(Graph *graph, int start_vertex)
 {
 	int i;
 	Edge *edge;
 	bool in_tree[MAX_VERTEX + 1];
-	int weights[MAX_VERTEX + 1];
+	int *distance = calloc(MAX_VERTEX + 1, sizeof(int));
 	int vertex, other_vertex;
-	int min_weight;
-	int total_weight;
+	int min_distance;
 
 	for(i = 1; i <= graph -> num_vertices; i++)
 	{
+		distance[i] = INF;	
 		in_tree[i] = false;
-		weights[i]= INF;
 		parent_vertex[i] = -1;
-	}	
+	}
 
-	total_weight = 0;
-	weights[start_vertex] = 0;
+	distance[start_vertex] = 0;
 	vertex = start_vertex;
 
 	while(!in_tree[vertex])
 	{
 		in_tree[vertex] = true;
+		
 		if(vertex != start_vertex)
-		{
 			printf("Edge (%d, %d) in tree\n", parent_vertex[vertex], vertex);
-			total_weight += min_weight;
-		}
-
+		
 		for(edge = graph -> edges[vertex]; edge; edge = edge -> next)
 		{
 			other_vertex = edge -> other_vertex;
-			if((weights[other_vertex] > edge -> weight) && (!in_tree[other_vertex])) 
+			if(distance[other_vertex] > distance[vertex] + edge -> weight)
 			{
-				weights[other_vertex] = edge -> weight;
+				distance[other_vertex] = distance[vertex] + edge -> weight;
 				parent_vertex[other_vertex] = vertex;
 			}
+			
 		}
-
-		min_weight = INF;	
+		
+		min_distance = INF;
 		for(i = 1; i <= graph -> num_vertices; i++)
 		{
-			if((!in_tree[i]) && (min_weight > weights[i]))
+			if(!in_tree[i] && (min_distance > distance[i]))
 			{
-				min_weight = weights[i];
+				min_distance = distance[i];
 				vertex = i;
 			}
 		}
 	}
 
-	return total_weight;
+	return distance;	
 }
 
 int main(int argc, char *argv[])
@@ -65,13 +62,17 @@ int main(int argc, char *argv[])
 	Graph graph;
 	bool directed = false;
 	int weight;
-
+	int i;
+	
 	make_graph(&graph, directed);
-	weight = prim(&graph, 1);
-
-	printf("Weight: %d\n", weight);	
+	int *distance = dijkstra(&graph, 1);
+	
+	for(i = 1; i <= graph.num_vertices; i++)
+		printf("Shortest distance from 1 to %d: %d\n", i, distance[i]);
 
 	destroy_graph(&graph);
+	free(distance);
+	distance = NULL;
 
 	return 0;
 }
